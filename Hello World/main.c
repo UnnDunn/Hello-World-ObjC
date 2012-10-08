22ad785176a5a0a34d3b38b8eab92c31af7322a1
Uchendu Nwachuku <developer@unndunn.com>
2012-10-08 18:05:48 -0400

    Initial Commit

---
+++ Hello World/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Hello World
+//
+//  Created by Uchendu Nwachuku on 10/8/12.
+//  Copyright (c) 2012 Uchendu Nwachuku. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
